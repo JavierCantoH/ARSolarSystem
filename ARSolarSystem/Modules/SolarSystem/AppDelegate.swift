b800a6f3ae35b7222afb9245ae76abbcdc4702be
Luis Javier Canto Hurtado <javier.canto@altomobile.io>
2023-06-14 11:29:19 -0600

    final

--- ARSolarSystem/Modules/SolarSystem/AppDelegate.swift
+++ ARSolarSystem/Modules/SolarSystem/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        //checkIfLogin()
-        presentTabBarVC()
+        checkIfLogin()
+        //presentTabBarVC()
         return true
     }
     
